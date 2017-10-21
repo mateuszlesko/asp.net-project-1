dd4565568161d16e3cd8a47d19e8e419e84c5c97
mateuszlesko <31209474+mateuszlesko@users.noreply.github.com>
2017-10-21 12:36:26 +0200

    opis
    RVSP by asp.net inspired book A. Freeman ASP.NET MVC5

---
+++ rvsp/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rvsp.Models;
+
+namespace rvsp.Controllers
+{
+    public class DefaultController : Controller
+    {
+      
+        // GET: Default
+        public ViewResult Index()
+        {
+            ViewBag.Day = DateTime.Now.DayOfWeek;
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RvspForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RvspForm(GuestResponse guestResponse)
+        {//Sprawdzanie bledow kontroli poprawnosci  formularza
+            if(ModelState.IsValid)
+            { 
+            return View("Thanks", guestResponse);
+            }
+            else//gdy warunek nie zostanie spelniony
+            {
+                
+                return View();
+            }
+        }
+    }
+}
---
+++ rvsp/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace rvsp.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Prosze podać prawidłowe imię")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Prosze podać prawidłowe nazwisko")]
+        public string Surname { get; set; }
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage ="Prosze podac prawidlowy adres email")]
+        public string Email{ get; set; }
+        [RegularExpression("^\\d+$",
+            ErrorMessage ="Prosze podac prawidlowy numer telefonu")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Prosze okreslic, czy wezmiesz udzial")]
+        public bool? WillAttend { get; set; }
+    }
+}
